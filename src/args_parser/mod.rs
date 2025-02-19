cf8a85847e4be88eddf454d5bd35b61e96e42d43
Cooper <xcooper1030@gmail.com>
2025-02-19 08:59:04 +0800

    adding parse init cmd args

--- src/args_parser/mod.rs
+++ src/args_parser/mod.rs
@@ -1,8 +1,19 @@
 use std::{error::Error, fmt::Display};
+use sub_parsers::parse_init_cmd_args;
+
+mod sub_parsers;
 
 #[derive(Debug)]
 pub struct ParsingError {
 	pub message: String
+}
+
+impl ParsingError {
+	fn new(message: String) -> ParsingError {
+		return ParsingError {
+			message: message,
+		}
+	}
 }
 
 impl Display for ParsingError {
@@ -15,13 +26,13 @@ impl Error for ParsingError {}
 
 pub enum Command {
 	Invalid,
-	Config,
+	Init,
 }
 
 impl From<&String> for Command {
 	fn from(value: &String) -> Self {
 		match value.to_lowercase().as_str() {
-			"config" => Command::Config,
+			"init" => Command::Init,
 			_ => Command::Invalid,
 		}
 	}
@@ -35,17 +46,25 @@ pub struct ParsedArgs {
 
 pub fn parse(args: &Vec<String>) -> Result<ParsedArgs, ParsingError> {
 	let mut parsed_args = ParsedArgs::default();
-	for arg in args {
-		match arg.as_str() {
-			"-h" | "--help" => {
-				parsed_args.is_help = true;
+	let arg = &args[0];
+	match arg.as_str() {
+		"-h" | "--help" => {
+			parsed_args.is_help = true;
+		},
+		_ => {
+			parsed_args.command = Some(arg.into());
+		},
+	}
+	return if let Some(cmd) = &parsed_args.command {
+		match cmd {
+			Command::Invalid => {
+				Err(ParsingError::new(format!("'{}' is not a valid command", arg)))
 			},
-			c => {
-				if !c.starts_with("-") {
-					parsed_args.command = Some(arg.into());
-				}
+			Command::Init => {
+				parse_init_cmd_args(&mut parsed_args)
 			},
 		}
+	} else {
+		panic!("must have a command")
 	}
-	return Ok(parsed_args);
 }
