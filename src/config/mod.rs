bbbe8f4bdd12b129e6ddf648fc4b59185cf4d324
Cooper <xcooper1030@gmail.com>
2025-03-05 00:11:52 +0800

    test cfg path ok

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,3 +1,7 @@
+use std::env;
+use std::io::{Error, ErrorKind};
+use std::path::PathBuf;
+
 pub enum Provider {
     OpenAi,
 }
@@ -20,4 +24,55 @@ pub fn load_config() -> Config {
 
 pub fn save_config() {
     todo!()
+}
+
+fn get_config_path() -> Result<PathBuf, std::io::Error> {
+    let os = env::consts::OS;
+    match os {
+        "linux" | "macos" => {
+            let xdg_cfg_home = env::var("XDG_CONFIG_HOME");
+            if let Ok(cfg_path) = xdg_cfg_home {
+                Ok(PathBuf::from(cfg_path).join("mobius/config.toml"))
+            } else {
+                Ok(PathBuf::from("~/.config/mobius/config.toml"))
+            }
+        }
+        "windows" => {
+            let appdata = env::var("APPDATA");
+            if let Ok(cfg_path) = appdata {
+                Ok(PathBuf::from(cfg_path).join("mobius\\config.toml"))
+            } else {
+                Err(Error::new(
+                    ErrorKind::NotFound,
+                    "%APPDATA% cannot be resolved",
+                ))
+            }
+        }
+        _ => Err(Error::new(ErrorKind::Unsupported, "OS not supported")),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_config_path() {
+        let os = env::consts::OS;
+        let path = get_config_path().unwrap();
+        match os {
+            "linux" | "macos" => {
+                assert_eq!(path.to_str().unwrap(), "~/.config/mobius/config.toml");
+            }
+            "windows" => {
+                assert_eq!(
+                    path.to_str().unwrap(),
+                    format!("{}\\mobius\\config.toml", env::var("APPDATA").unwrap())
+                );
+            }
+            _ => {
+                panic!("OS not supported");
+            }
+        }
+    }
 }
