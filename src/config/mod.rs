519ae622373c040a8e07018ee3b3bbdb17995959
Cooper <xcooper1030@gmail.com>
2025-03-09 01:41:11 +0800

    config mod ok

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,11 +1,17 @@
+use serde::{Deserialize, Serialize};
 use std::env;
+use std::fs::read_to_string;
+use std::fs::write;
 use std::io::{Error, ErrorKind};
 use std::path::PathBuf;
+use toml::{from_str, to_string};
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub enum Provider {
     OpenAi,
 }
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct LLM {
     pub provider: Provider,
     pub model: String,
@@ -14,19 +20,23 @@ pub struct LLM {
     pub temperature: f32,
 }
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Config {
     pub llm: LLM,
 }
 
-pub fn load_config() -> Config {
-    todo!()
+pub fn load_config(cfg_path: &PathBuf) -> Result<Config, Error> {
+    let content = read_to_string(cfg_path)?;
+    let config: Config = from_str(&content).unwrap();
+    Ok(config)
 }
 
-pub fn save_config() {
-    todo!()
+pub fn save_config(config: &Config, cfg_path: &PathBuf) -> Result<(), Error> {
+    write(cfg_path, to_string(config).unwrap())?;
+    Ok(())
 }
 
-fn get_config_path() -> Result<PathBuf, std::io::Error> {
+pub fn get_config_path() -> Result<PathBuf, Error> {
     let os = env::consts::OS;
     match os {
         "linux" | "macos" => {
@@ -74,5 +84,22 @@ mod tests {
                 panic!("OS not supported");
             }
         }
+    }
+
+    #[test]
+    fn test_read_write_config() {
+        let config = Config {
+            llm: LLM {
+                provider: Provider::OpenAi,
+                model: "gpt-3.5-turbo".to_string(),
+                api_key: Some("test".to_string()),
+                url: None,
+                temperature: 0.7,
+            },
+        };
+        let tmp_cfg_file = env::temp_dir().join("config.toml");
+        save_config(&config, &tmp_cfg_file).expect("Failed to save config");
+        let loaded_config = load_config(&tmp_cfg_file).expect("Failed to load config");
+        assert_eq!(config, loaded_config);
     }
 }
