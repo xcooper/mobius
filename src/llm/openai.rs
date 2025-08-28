5214b89e67bcee2b27edde20cbec8cb17757cc30
Cooper <xcooper1030@gmail.com>
2025-08-28 08:31:02 +0800

    use rig framework in openai impl

--- src/llm/openai.rs
+++ src/llm/openai.rs
@@ -6,14 +6,21 @@ use crate::llm::LLM;
 use async_trait::async_trait;
 use chrono::serde::ts_milliseconds;
 use chrono::{DateTime, Utc};
-use reqwest::Client;
+use rig::client::CompletionClient;
+use rig::completion::{CompletionModel, CompletionRequest, CompletionRequestBuilder};
+use rig::message::{Text, UserContent};
+use rig::OneOrMany;
 use serde::{Deserialize, Serialize};
+
+use rig::providers::openai::Client as RigClient;
+use rig::completion::Message as RigMessage;
+use rig::completion::Message::User;
+use rig::completion::message::AssistantContent;
 
 const DEFAULT_URL: &str = "https://api.openai.com/v1";
 
 pub struct OpenAI<'a> {
     config: &'a Config,
-    client: Client,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -55,7 +62,6 @@ impl<'a> OpenAI<'a> {
     pub fn new(config: &'a Config) -> Self {
         OpenAI {
             config,
-            client: reqwest::Client::new(),
         }
     }
 }
@@ -68,37 +74,40 @@ impl LLM for OpenAI<'_> {
         user_prompts: Vec<&str>,
     ) -> Result<String, Box<dyn Error>> {
         let llm = &self.config.llm;
-        let default_url = &DEFAULT_URL.to_string();
-        let url = llm.url.as_ref().unwrap_or(default_url);
         let api_key = llm.api_key.as_ref().unwrap();
         let model = llm.model.clone();
-        let input = concat_msgs(system_prompt, user_prompts);
-        let req = self
-            .client
-            .post(format!("{url}/responses"))
-            .bearer_auth(api_key)
-            .header("Content-Type", "application/json")
-            .json(&OpenAIRequest {
-                model: Some(model),
-                input: input,
+        let client = RigClient::new(&api_key);
+        let gpt = client.completion_model(model.as_str());
+        let req = CompletionRequest { 
+            preamble: Some(system_prompt.to_string()),
+            chat_history: convert_to_messages(user_prompts),
+            documents: Vec::new(),
+            tools: Vec::new(),
+            temperature: Some(1.0),
+            max_tokens: None,
+            additional_params: None,
+        };
+        let resp = gpt.completion(req);
+        resp.await
+            .map(|r| r.choice)
+            .map(|o| o.first())
+            .map(|a| match a {
+                AssistantContent::Text(text) => text.text,
+                AssistantContent::ToolCall(tool_call) => todo!(),
+                AssistantContent::Reasoning(reasoning) => todo!(),
             })
-            .build()?;
-        let resp: OpenAIResponse = self.client.execute(req).await?.json().await?;
-        Ok(resp.output.unwrap()[0].content[0].text.clone())
+            .map_err(|ce| Box::from(ce))
     }
 }
 
-fn concat_msgs(sys_prompt: &str, user_prompts: Vec<&str>) -> Vec<Message> {
-    let mut msgs: Vec<Message> = Vec::new();
-    msgs.push(Message {
-        role: super::Role::System,
-        content: sys_prompt.to_string(),
-    });
-    for user_prompt in user_prompts {
-        msgs.push(Message {
-            role: super::Role::User,
-            content: user_prompt.to_string(),
-        });
-    }
-    return msgs;
+fn convert_to_messages(user_prompts: Vec<&str>) -> OneOrMany<RigMessage> {
+    let rig_msgs: Vec<RigMessage> = user_prompts
+        .iter()
+        .map(|&p| User {
+            content: OneOrMany::one(
+                UserContent::Text(Text { text: p.to_string() })
+            ) 
+        })
+        .collect();
+    OneOrMany::many(rig_msgs).unwrap()
 }
